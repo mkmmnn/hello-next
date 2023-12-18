651688eb7d9757f98b9f230ef9a434cc4c77787d
mkmmnn <kammannmolly@gmail.com>
2023-12-17 19:36:14 -0500

    keep track of hits in a lil json db

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React from "react";
+import { readFile, writeFile } from "./helpers/file-helpers";
 
 function Home() {
+  const content = JSON.parse(readFile("src/app/database.json"));
+  const nextContent = { hits: content.hits + 1 };
+  writeFile("src/app/database.json", JSON.stringify(nextContent));
   return (
     <main>
-      <h1>Hello Next!</h1>
+      <h1>page visitors: {content.hits}</h1>
     </main>
   );
 }
